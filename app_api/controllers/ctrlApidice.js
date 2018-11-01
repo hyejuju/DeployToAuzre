d816c210d55cd5c86a569e0a32a42660b6827e44
hyejuju <42934129+hyejuju@users.noreply.github.com>
2018-11-01 14:25:41 +0200

    isitdone

--- app_api/controllers/ctrlApidice.js
+++ app_api/controllers/ctrlApidice.js
@@ -3,14 +3,27 @@ const mongoose = require('mongoose');
 const diceModel = mongoose.model('dicemodelname');
 
 const winnerlist = function (req,res) {
-    res
-        .status(200)
-        .json({"year" : "game"});
-
+    diceModel.find({}, function(err, winners){
+        if (err){
+            res.status(404).json(err);
+        }
+        else{
+            res.status(200).json(winners);
+        }
+        });
 };
 
 const addWinner = function (req,res) {
-    res.status(201).json({"Add winner" : "Work in progress"});
+    //res.status(201).json({"Add winner" : "Work in progress"});
+    diceModel.create(req.body, function(err, newWinner){
+        if (err){
+            res.status(400).json(err);
+        }
+        else{
+            res.status(201).json(newWinner);
+        }
+        }
+    );
 };
 
 module.exports = {
