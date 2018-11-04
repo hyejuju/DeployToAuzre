8b210ff7142ac0d94e318ea18593e777dc814f78
hyejuju <42934129+hyejuju@users.noreply.github.com>
2018-11-04 22:52:55 +0200

    get

--- app_server/controllers/ctrlBritish.js
+++ app_server/controllers/ctrlBritish.js
@@ -1,6 +1,36 @@
+const request = require('request');
+const apiURL = require('./apiURLs');
 
 const winnerlist = function(req, res){
-    res.render('british',{
+    const path = '/api/british';//is it british??
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+    request(
+        requestOptions,
+        function (err, response, body) {
+            if (err) {
+                res.render('error', {message: err.message});
+            }
+            else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' + response.statusMessage + " ("+ response.statusCode + ")" });
+            }
+            else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            }
+            else if (!body.length){
+                res.render('error', {message: 'No dicuments in collection'});
+            }
+            else {
+                res.render('british', {winners: body});//is it british
+            }
+        }
+    );
+  /*  res.render('british',{
         winners:
         [
             {year:'2010', game:'Mass Effect 2', team: 'BioWare'},
@@ -11,7 +41,7 @@ const winnerlist = function(req, res){
             {year:'2015', game:'Fallout 4', team: 'Bethesda Game Studios'},
             {year:'2016', game:'Uncharted 4: A Thiefs End', team: 'Naughty Dog'},
             {year:'2017', game:'What Remains of Edith Finch', team: 'Giant Sparrow'}
-        ]});
+        ]});*/
 };
 
 module.exports = {
--- app_server/controllers/ctrlDICE.js
+++ app_server/controllers/ctrlDICE.js
@@ -1,6 +1,34 @@
 
 const winnerlist = function(req, res){
-    res.render('dice',{
+    const path = '/api/dice';//is it??
+    const requestOptions = {
+        url : apiURL.server + path,
+        method : 'GET',
+        json : {},
+        qs : {}
+    };
+
+    request(
+        requestOptions,
+        function (err, response, body) {
+            if (err) {
+                res.render('error', {message: err.message});
+            }
+            else if (response.statusCode !== 200){
+                res.render('error', {message: 'Error accessing API: ' + response.statusMessage + " ("+ response.statusCode + ")" });
+            }
+            else if (!(body instanceof Array)) {
+                res.render('error', {message: 'Unexpected response data'});
+            }
+            else if (!body.length){
+                res.render('error', {message: 'No dicuments in collection'});
+            }
+            else {
+                res.render('dice', {winners: body});
+            }
+        }
+    );
+    /*res.render('dice',{
         winners:
         [
             {year:'2010', game:'Mass Effect 2', team: 'BioWare'},
@@ -11,7 +39,7 @@ const winnerlist = function(req, res){
             {year:'2015', game:'Fallout 4', team: 'Bethesda Game Studios'},
             {year:'2016', game:'Overwatch', team: 'Blizzard Entertainment'},
             {year:'2017', game:'The Legend of Zelda:Breath of the wild', team: 'Nintendo EPD'}
-        ]});
+        ]});*/
 };
 module.exports = {
     winnerlist
