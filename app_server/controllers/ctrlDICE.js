ae6852d72a90ee648107a98a171ca5bfe766d2a5
hyejuju <42934129+hyejuju@users.noreply.github.com>
2018-11-04 22:59:11 +0200

    get done

--- app_server/controllers/ctrlDICE.js
+++ app_server/controllers/ctrlDICE.js
@@ -1,6 +1,8 @@
+const request = require('request');
+const apiURL = require('./apiURLs');
 
 const winnerlist = function(req, res){
-    const path = '/api/dice';//is it??
+    const path = '/api/dice';
     const requestOptions = {
         url : apiURL.server + path,
         method : 'GET',
@@ -28,18 +30,7 @@ const winnerlist = function(req, res){
             }
         }
     );
-    /*res.render('dice',{
-        winners:
-        [
-            {year:'2010', game:'Mass Effect 2', team: 'BioWare'},
-            {year:'2011', game:'The Elder Scrolls V: skyrim', team: 'Bethesda Game Studios'},
-            {year:'2012', game:'Journey', team: 'Thatgamecompany'},
-            {year:'2013', game:'The Last of US', team: 'Naughty Dog'},
-            {year:'2014', game:'Dragon Age: Inquisition', team: 'BioWare'},
-            {year:'2015', game:'Fallout 4', team: 'Bethesda Game Studios'},
-            {year:'2016', game:'Overwatch', team: 'Blizzard Entertainment'},
-            {year:'2017', game:'The Legend of Zelda:Breath of the wild', team: 'Nintendo EPD'}
-        ]});*/
+
 };
 module.exports = {
     winnerlist
